9fe1369ee5330a1ba123dd870c9e77ef6ef68c37
Ethan White <ethan@weecology.org>
2018-07-29 22:21:43 -0400

    Remove old Jupyter and MODIStools installations
    These are no longer used

--- install.R
+++ install.R
@@ -2,22 +2,14 @@
 
 if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
 
-
 # Install analysis packages using pacman
 
 pacman::p_load(dplyr, forecast, ggplot2, Hmisc, tidyr, mgcv, sp, raster,
-               maptools, doParallel, stringr, RCurl, roxygen2,
-	             broom, devtools, doParallel, dplyr, forecast, ggplot2,
-	             gimms, Hmisc, maptools, mgcv, prism, raster, stringr, sp,
+               maptools, doParallel, stringr, RCurl, roxygen2, rdataretriever,
+               broom, devtools, doParallel, dplyr, forecast, ggplot2,
+               gimms, Hmisc, maptools, mgcv, prism, raster, stringr, sp,
                tidyr, rgdal, rgeos, DBI, RSQLite, lme4, caret, mapproj,
                viridis, git2r, rstan, readr, purrr, gbm, randomForest,
                purrrlyr, ggjoy
 	       )
-pacman::p_load_gh('seantuck12/MODISTools', 'ropensci/rdataretriever')
-
-
-# Install IRkernal packages and spec
-
-pacman::p_load(repr, pbdZMQ)
-pacman::p_load_gh('IRkernel/IRdisplay', 'IRkernel/IRkernel')
-IRkernel::installspec()
+pacman::p_load_gh('ropensci/rdataretriever')
